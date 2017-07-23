b83f592201607969eb7a28c92a1fb8c839babbdc
tombe0vt <tombe0.vt93@gmail.com>
2017-07-23 14:36:03 +0700

    commit changed

---
+++ app/src/main/java/thanh/com/bai3githubversion2/LoginActivity.java
@@ -0,0 +1,10 @@
+package thanh.com.bai3githubversion2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by THANH on 23/07/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/thanh/com/bai3githubversion2/Studen.java
@@ -0,0 +1,8 @@
+package thanh.com.bai3githubversion2;
+
+/**
+ * Created by THANH on 23/07/2017.
+ */
+
+public class Studen {
+}
